f22cb5dfc90e0ef073388fda6bc4bb1ab0514f6d
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-04-02 04:52:59 -0700

    Initial commit

---
+++ main.ts
@@ -0,0 +1,34 @@
+
+import inquirer from "inquirer";
+import chalk, { Chalk } from "chalk";
+
+let todoList = [];
+let conditions = true;
+
+console.log(chalk.green.bold("\n\t Welcome to To-do List Application:\n"))
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name:"task",
+            type:"input",
+            message:chalk.red("Enter your New Task:"),
+
+        }
+    ]);
+    todoList.push(addTask.task)
+    console.log( chalk.yellow ( addTask.task ) +  " :Task added in todo List Successfully ")
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name:"addmore",
+            type:"confirm",
+            message:chalk.red("Do you want to add more task ?"),
+            default:"false"
+        }
+    ]);
+
+    conditions = addMoreTask.addmore
+}
+
+console.log(chalk.yellow("Your updated todo list"), todoList)
